e6aabc4dc18ad7cf32f732fc3ffba1cbf258384f
Bartosz Piwowarczyk <b.piwowarczyk@gmail.com>
2025-07-09 15:34:44 +0200

    Implemented per-user-authorization

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
+  
 
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
@@ -29,6 +30,7 @@ function App() {
   return (
     <main>
       <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo => <li 
